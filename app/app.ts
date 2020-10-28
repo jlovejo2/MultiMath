070cda4a5ff3e42de39d00825a0aa9af6717cf26
James Lovejoy <james.lovejoy2@gmail.com>
2020-10-28 17:23:01 -0500

    added logMessage and logError message, set thos to variable that is called in postScore

--- app/app.ts
+++ app/app.ts
@@ -4,7 +4,8 @@ function startGame() {
     const playerName: string | undefined = getInputValue('playername')
     logPlayer(playerName);
 
-    postScore(100)
+    postScore(100, playerName)
+    postScore(-5)
 
 }
 
@@ -23,8 +24,25 @@ function getInputValue (elementID: string): string | undefined {
 }
 
 function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+    
+    let logger: (value: string) => void;
+    
+    if (score < 0 ) {
+        logger = logError
+    } else {
+        logger = logMessage;
+    }
+
     const scoreElement: HTMLElement | null = document.getElementById('postedScores');
     scoreElement!.innerText = `${score} - ${playerName}`
+
+    logger( `Score: ${score}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+const logMessage = (message: string) =>  console.log(message);
+
+function logError(err: string): void {
+    console.error(err);
+}
