79bd083d78a5ecd512b9bc467ac5441d364adb67
James Lovejoy <james.lovejoy2@gmail.com>
2020-10-28 17:03:21 -0500

    added getInputValue function

--- app/app.ts
+++ app/app.ts
@@ -8,8 +8,18 @@ function startGame() {
     messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
 }
 
-function logPlayer (name: string) {
+function logPlayer (name: string): void {
     console.log(`New game starting for player ${name}`);
+}
+
+function getInputValue (elementID: string): string | undefined {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+    if (inputElement.value === '') {
+        return undefined;
+    } else {
+        return inputElement.value
+    }
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
