e50c61556d2f572e63075a8f5e28c8d1056300ee
James Lovejoy <james.lovejoy2@gmail.com>
2020-10-29 06:11:30 -0500

    added display game method to game class

--- app/game.ts
+++ app/game.ts
@@ -5,15 +5,29 @@
 
 class Game {
     private scoreboard: Scoreboard = new Scoreboard();
-    player: Player;
-    problemCount: number;
-    factor: number;
 
-    constructor(newPlayer: Player, numOfProblems: number, multFactor: number) {
-        this.player = newPlayer;
-        this.problemCount = numOfProblems;
-        this.factor = multFactor;
+    constructor(public player: Player, public problemCount: number, public factor: number) {
+
     }
+
+    //displays html for game board on screen
+    displayGame() {
+        let gameForm: string = '';
+        for (let i = 1; i <= this.problemCount; i++) {
+            gameForm += '<div class="form-group">'
+            gameForm += '<label for="answer' + i + '" class="col-sm-2 control-label">';
+            gameForm += String(this.factor) + ' X ' + i + ' = </label>';
+            gameForm += '<div class="col-sm-1"><input type="text" class="form-control" id="answer"' + i + '" size="5" /></div>';
+            gameForm += '</div>';
+        }
+
+    //add a new game to the page
+    const gameElement: HTMLElement = document.getElementById('game')
+    gameElement.innerHTML = gameForm;
+
+    //enable calculate button
+    document.getElementById('calculate')!.removeAttribute('disabled');
+}
 }
 
 
