0e4e9fa3d16c10cc130d8944a26d7dd1236e42e9
James Lovejoy <james.lovejoy2@gmail.com>
2020-10-28 19:36:41 -0500

    created player class

---
+++ app/player.ts
@@ -0,0 +1,17 @@
+class Player implements Person {
+    name: string;
+    age?: number;
+    highScore: number;
+
+    constructor() {
+
+        this.name = '';
+        this.highScore = 0;
+        
+    }
+
+    formatName () {
+        return this.name.toUpperCase()
+    }
+
+}
