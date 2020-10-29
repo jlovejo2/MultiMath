796177ec153612ff4382d4c73577b964a01b4a1b
James Lovejoy <james.lovejoy2@gmail.com>
2020-10-29 06:57:51 -0500

    added conditional statements to scoreboard based on score

--- app/scoreboard.ts
+++ app/scoreboard.ts
@@ -14,8 +14,18 @@ class Scoreboard {
         for (let index = 0; index < this.results.length; index++) {
             const result: Result = this.results[index];
             output += '<h4>';
-            output += result.playerName + ':' + result.score + '/' + result.problemCount + 'for factor' + result.factor;
+            output += result.playerName + ': ' + result.score + '/' + result.problemCount + ' for factor ' + result.factor;
             output += '</h4>';
+            if (result.score === result.problemCount) {
+            output += '<hr/>';
+            output += '<h3>Great job! Perfect Score!!!</h3>'
+            } else if ( result.score === 0 ){
+                output += '<hr/>';
+                output += '<h3>Yikes, time to hit those books.</h3>'        
+            } else {
+                output += '<hr/>';
+                output += '<h3>Thanks for playing!</h3>'
+            }
         }
 
         const scoreElement: HTMLElement = document.getElementById('scores')!;
